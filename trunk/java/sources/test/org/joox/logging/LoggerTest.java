2ca20a9e6e69249e4fe6dc3943281822c6067b88
berth <berth@f37073c5-78ec-0310-bf1b-d0d530c269eb>
2006-07-18 11:01:54 +0000

    Added items remotely
    J:\workspace\joox\java\sources\test\org\joox\logging\LoggerTest.java

---
+++ trunk/java/sources/test/org/joox/logging/LoggerTest.java
@@ -0,0 +1,310 @@
+/***************************************************************************************************************************************************************
+ * Copyright (c) 2001, 2004 IBM Corporation and others. All rights reserved. This program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: IBM Corporation - initial API and implementation
+ **************************************************************************************************************************************************************/
+/**
+ * 
+ */
+package org.joox.test.logging;
+
+import junit.framework.TestCase;
+
+/**
+ * @author bert
+ * 
+ */
+public class LoggerTest extends TestCase
+{
+
+	/**
+	 * Constructor for LoggerTest.
+	 * 
+	 * @param arg0
+	 */
+	public LoggerTest(String arg0)
+	{
+		super(arg0);
+	}
+
+	/**
+	 * @see TestCase#setUp()
+	 */
+	@Override
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+	}
+
+	/**
+	 * @see TestCase#tearDown()
+	 */
+	@Override
+	protected void tearDown() throws Exception
+	{
+		super.tearDown();
+	}
+
+	/**
+	 * Test method for 'org.joox.logging.AbstractLogger.AbstractLogger(Class)'
+	 */
+	public void testAbstractLogger()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Test method for 'org.joox.logging.AbstractLogger.in(int, String)'
+	 */
+	public void testInIntString()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Test method for 'org.joox.logging.AbstractLogger.in(String)'
+	 */
+	public void testInString()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Test method for 'org.joox.logging.AbstractLogger.out(int, Object)'
+	 */
+	public void testOutIntObject()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Test method for 'org.joox.logging.AbstractLogger.out(Object)'
+	 */
+	public void testOutObject()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Test method for 'org.joox.logging.AbstractLogger.err(String, Throwable)'
+	 */
+	public void testErr()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Test method for 'org.joox.logging.AbstractLogger.ext()'
+	 */
+	public void testExt()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Test method for 'org.joox.logging.AbstractLogger.println(int)'
+	 */
+	public void testPrintln()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Test method for 'org.joox.logging.AbstractLogger.out()'
+	 */
+	public void testOut()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Test method for 'org.joox.logging.AbstractLogger.out(int)'
+	 */
+	public void testOutInt()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Test method for 'org.joox.logging.AbstractLogger.inf(String)'
+	 */
+	public void testInf()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Test method for 'org.joox.logging.AbstractLogger.msg(String)'
+	 */
+	public void testMsg()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Test method for 'org.joox.logging.AbstractLogger.dbg(String)'
+	 */
+	public void testDbg()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Test method for 'org.joox.logging.AbstractLogger.wrn(String)'
+	 */
+	public void testWrn()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Test method for 'org.joox.logging.AbstractLogger.var(String, Object)'
+	 */
+	public void testVarStringObject()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Test method for 'org.joox.logging.AbstractLogger.var(String, int)'
+	 */
+	public void testVarStringInt()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Test method for 'org.joox.logging.AbstractLogger.var(String, char)'
+	 */
+	public void testVarStringChar()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Test method for 'org.joox.logging.AbstractLogger.var(String, byte)'
+	 */
+	public void testVarStringByte()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Test method for 'org.joox.logging.AbstractLogger.var(String, boolean)'
+	 */
+	public void testVarStringBoolean()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Test method for 'org.joox.logging.AbstractLogger.var(String, short)'
+	 */
+	public void testVarStringShort()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Test method for 'org.joox.logging.AbstractLogger.var(String, long)'
+	 */
+	public void testVarStringLong()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Test method for 'org.joox.logging.AbstractLogger.var(String, float)'
+	 */
+	public void testVarStringFloat()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Test method for 'org.joox.logging.AbstractLogger.var(String, double)'
+	 */
+	public void testVarStringDouble()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Test method for 'org.joox.logging.AbstractLogger.format(String, Object)'
+	 */
+	public void testFormat()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Test method for 'org.joox.logging.AbstractLogger.getInset()'
+	 */
+	public void testGetInset()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Test method for 'org.joox.logging.AbstractLogger.getStack()'
+	 */
+	public void testGetStack()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Test method for 'org.joox.logging.AbstractLogger.newData()'
+	 */
+	public void testNewData()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Test method for 'org.joox.logging.AbstractLogger.getMethod()'
+	 */
+	public void testGetMethod()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Test method for 'org.joox.logging.AbstractLogger.getThread1()'
+	 */
+	public void testGetThread1()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
