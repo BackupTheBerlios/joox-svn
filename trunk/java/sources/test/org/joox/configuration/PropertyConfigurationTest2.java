9945b9a5bec5c1ba8a54d4233629570fe89c780e
berth <berth@f37073c5-78ec-0310-bf1b-d0d530c269eb>
2006-07-18 10:58:41 +0000

    Added items remotely
    J:\workspace\joox\java\sources\test\org\joox\configuration\PropertyConfigurationTest2.java

---
+++ trunk/java/sources/test/org/joox/configuration/PropertyConfigurationTest2.java
@@ -0,0 +1,213 @@
+/***************************************************************************************************************************************************************
+ * Copyright (c) 2001, 2004 IBM Corporation and others. All rights reserved. This program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: IBM Corporation - initial API and implementation
+ **************************************************************************************************************************************************************/
+/*
+ * Created on Dec 16, 2004
+ * 
+ * TODO To change the template for this generated file go to Window - Preferences - Java - Code Style - Code Templates
+ */
+package org.joox.configuration;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Hoevbert
+ * 
+ * TODO To change the template for this generated type comment go to Window - Preferences - Java - Code Style - Code Templates
+ */
+public class PropertyConfigurationTest2 extends TestCase
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// org.junit.runners.AllTests
+		// junit.ui.run(PropertyConfigurationTest.class);
+	}
+
+	/**
+	 * @see TestCase#setUp()
+	 */
+	@Override
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+	}
+
+	/**
+	 * @see TestCase#tearDown()
+	 */
+	@Override
+	protected void tearDown() throws Exception
+	{
+		super.tearDown();
+	}
+
+	/**
+	 * Constructor for PropertyConfigurationTest.
+	 * 
+	 * @param arg0
+	 */
+	public PropertyConfigurationTest2(String arg0)
+	{
+		super(arg0);
+	}
+
+	/**
+	 * 
+	 */
+	public final void testMain()
+	{
+		// TODO Implement main().
+	}
+
+	/**
+	 * Class under test for Singleton instance(String)
+	 */
+	public final void testInstanceString()
+	{
+		// TODO Implement instance().
+	}
+
+	/**
+	 * 
+	 */
+	public final void testRun()
+	{
+		// TODO Implement run().
+	}
+
+	/**
+	 * 
+	 */
+	public final void testReset()
+	{
+		// TODO Implement reset().
+	}
+
+	/**
+	 * 
+	 */
+	public final void testGet()
+	{
+		// TODO Implement get().
+	}
+
+	/**
+	 * 
+	 */
+	public final void testSet()
+	{
+		// TODO Implement set().
+	}
+
+	/**
+	 * 
+	 */
+	public final void testGetKeys()
+	{
+		// TODO Implement getKeys().
+	}
+
+	/**
+	 * 
+	 */
+	public final void testStore()
+	{
+		// TODO Implement store().
+	}
+
+	/**
+	 * Class under test for Singleton instance()
+	 */
+	public final void testInstance()
+	{
+		// TODO Implement instance().
+	}
+
+	/**
+	 * 
+	 */
+	public final void testObservable()
+	{
+		// TODO Implement Observable().
+	}
+
+	/**
+	 * 
+	 */
+	public final void testAddObserver()
+	{
+		// TODO Implement addObserver().
+	}
+
+	/**
+	 * 
+	 */
+	public final void testDeleteObserver()
+	{
+		// TODO Implement deleteObserver().
+	}
+
+	/**
+	 * Class under test for void notifyObservers()
+	 */
+	public final void testNotifyObservers()
+	{
+		// TODO Implement notifyObservers().
+	}
+
+	/**
+	 * Class under test for void notifyObservers(java.lang.Object)
+	 */
+	public final void testNotifyObserversObject()
+	{
+		// TODO Implement notifyObservers().
+	}
+
+	/**
+	 * 
+	 */
+	public final void testDeleteObservers()
+	{
+		// TODO Implement deleteObservers().
+	}
+
+	/**
+	 * 
+	 */
+	public final void testSetChanged()
+	{
+		// TODO Implement setChanged().
+	}
+
+	/**
+	 * 
+	 */
+	public final void testClearChanged()
+	{
+		// TODO Implement clearChanged().
+	}
+
+	/**
+	 * 
+	 */
+	public final void testHasChanged()
+	{
+		// TODO Implement hasChanged().
+	}
+
+	/**
+	 * 
+	 */
+	public final void testCountObservers()
+	{
+		// TODO Implement countObservers().
+	}
+
+}
